f9bead241a1a9f223eebb1cb52060636cc3de8a2
Baran Yıldırım <xbyildirim@gmail.com>
2023-07-20 11:49:14 +0300

    product list swiper development

--- js/_main.js
+++ js/_main.js
@@ -3,3 +3,18 @@ var swiper = new Swiper(".swipType1", {
     slidesPerView: 4,
     spaceBetween: 0,
 });
+
+var swipPrdct1 = new Swiper(".swipPrdct1",{
+    slidesPerView: 4,
+    spaceBetween: 0,
+    loop:false,
+    autoplay: {
+        delay: 1000,
+      },
+});
+$('.prListGrid').on('mouseenter', function(e){
+    swipPrdct1.autoplay.stop();
+})
+$('.prListGrid').on('mouseleave', function(e){    
+    swipPrdct1.autoplay.start();
+}) 
